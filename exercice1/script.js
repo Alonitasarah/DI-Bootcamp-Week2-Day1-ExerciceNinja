12451debc6994ebd4975bea128e421d705b446c4
Alonitasarah <121178294+Alonitasarah@users.noreply.github.com>
2023-01-10 19:09:43 +0000

    Add files via upload

---
+++ exercice1/script.js
@@ -0,0 +1,29 @@
+// Prediction : True, car 5 est supérieur à 1
+5 >= 1;
+
+// Prediction : False, car 0 est inférieur à 1
+0 === 1; 
+
+// Prediction : False, car 4 n'est pas inferieur à 1
+4 <= 1; 
+
+//Prediction : false, car 1 n'est pas different de 1
+1 != 1; 
+
+// Prediction : true, car l'emplacement en memoire de A est supérieur à pour B
+"A" > "B"; 
+
+// Prediction : true, car l'emplacement en memoire de C est supérieur à pour B
+"B" < "C"; 
+
+// Prediction : true, car l'emplacement en memoire de a est supérieur à pour A
+"a" > "A"; 
+
+// Prediction : false, car l'emplacement en memoire de b est supérieur à pour A
+"b" < "A" 
+
+// Prediction : false
+true === false; 
+
+// Prediction : false
+true != true;
